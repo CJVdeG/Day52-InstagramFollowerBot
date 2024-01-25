730646b6f03526b0fba77969d7b3d81554e3725e
CJVdeG <chris@theincitement.com>
2024-01-25 22:45:55 +0800

    added the solution of the project

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bot import InstaFollower
 import time
 
 # TODO: Insert which Instagram account you want to follow, e.g.:
-ig_account = 'ai_artimagecreator'
+ig_account = 'aiartmagnet'
 
 # TODO: Instantiate the bot
 bot = InstaFollower()
